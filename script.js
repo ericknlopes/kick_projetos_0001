168ef091492080ecbb4461b0cab45f8bd6764d9f
ericknlopes <78979694+ericknlopes@users.noreply.github.com>
2024-06-14 00:39:04 -0300

    Desafio parada 23 - construção de busca
    Em construção html , css e js

---
+++ script.js
@@ -0,0 +1,11 @@
+const items = {
+    'banana': 1.5,
+    'maçã': 2.0,
+    'laranja': 2.5,
+    'uva': 3.0,
+    'manga': 4.0
+};
+
+// Função para obter o valor do item
+function getItemValue() {
+    const itemInput = document.getElementById
